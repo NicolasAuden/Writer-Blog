aef5a77b8df6608221e3444a930b596a8fa2eb0d
Nicolas <nicolas.auden@gmail.com>
2018-11-16 18:23:17 +0100

    html

---
+++ layout/layout.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <title><?php echo $title ?></title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    </head>
+  <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <a class="navbar-brand" href="index.php">Jean FORTEROCHE</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarColor01">
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Accueil<span class="sr-only">(current)</span></a>
+                </li>
+                
+               
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?action=signIn">Se connecter</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?action=signUp">S'inscrire</a>
+                </li>
+               <li class="nav-item">
+                    <a class="nav-link" href="index.php?action=dashboard">Tableau de bord</a>
+                </li>
+                
+                </ul>
+                
+            </div>
+        </nav>
+    </header>
+
+    
+
+    <!-- <div class="container">
+        <div class="row">
+            <div class="col-12"> -->
+               
+            <?php include($vue) ?>
+            
+            <!-- </div>
+        </div>
+    </div> -->
+
+    
+
+    <footer class="footer">
+      
+    </footer>
+
+
+           
+    
+    
+
+  </body>
+</html>
