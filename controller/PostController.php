a2dd99a794ae511ba4cf2ccc9855ab510e6aea1b
Nicolas <nicolas.auden@gmail.com>
2018-11-17 12:24:49 +0100

    Post methods

--- controller/PostController.php
+++ controller/PostController.php
@@ -5,5 +5,12 @@
 //models
 include_once("./model/Post.php");
 
+function getAllPosts(){
+    $post = new Post();
+    $listPosts = $post->allPosts();
+
+    return $listPosts;
+}
+
 
 ?>
--- index.php
+++ index.php
@@ -1,38 +1,15 @@
 <?php
 
 session_start();
-require '../app/Autoloader.php';
-App\Autoloader::register();
-
-$app = App\App::getInstance();
-
-
-$posts = $app->getTable('Posts');
-$posts->all();
-
-
-
-
-
-
-
-/*if(isset($_GET['p'])){
-    $p = $_GET['p'];
-} else {
-    $p = 'home';
-}
-
 ob_start();
-if($p === 'home') {
-    require '../pages/home.php';
-} elseif ($p === 'article') {
-    require '../pages/single.php';
-}elseif ($p === 'category') {
-    require '../pages/categorie.php';
-}
 
-$content = ob_get_clean();
-require '../pages/templates/default.php';*/
+
+//includes
+include_once("model/BddConnect.php");
+
+include_once("layout/layout.php");
+
+ob_end_flush();
 
 ?>
 
